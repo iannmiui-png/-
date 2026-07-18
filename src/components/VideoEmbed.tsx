cb9f0b5af1d321f6ad6f251f5226610e60d061c1
iannmiui-png <i.ann.miui@gmail.com>
2026-07-17 18:30:27 -0600

    Remove referrerPolicy from VideoEmbed component
    Removed referrerPolicy attribute from VideoEmbed component.

--- src/components/VideoEmbed.tsx
+++ src/components/VideoEmbed.tsx
@@ -60,7 +60,6 @@ export default function VideoEmbed({ lang }: VideoEmbedProps) {
               className="absolute top-0 left-0 w-full h-full"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
-              referrerPolicy="no-referrer"
             />
           </div>
 
